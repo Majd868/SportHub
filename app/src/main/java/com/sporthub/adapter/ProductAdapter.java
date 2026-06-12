7509c0b22e2bd1619bf9794940644615445d2762
Majd868 <emmajd83@gmail.com>
2026-06-12 09:33:54 +0300

    يخىث

--- app/src/main/java/com/sporthub/adapter/ProductAdapter.java
+++ app/src/main/java/com/sporthub/adapter/ProductAdapter.java
@@ -11,7 +11,7 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
+ import com.bumptech.glide.Glide;
 import com.google.android.material.button.MaterialButton;
 import com.sporthub.R;
 import com.sporthub.data.model.Product;
